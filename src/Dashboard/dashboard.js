cc1716fc45bd4def21c20edaae8bb9df8c92e72f
CamilaSolis <camila.sol.cal@gmail.com>
2020-02-24 15:10:48 -0300

    first commit

---
+++ src/Dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import './dashboard.scss';
+
+class Dashboard extends component{
+  render(){
+    return(
+      <div>
+        <h1>oli</h1>
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
